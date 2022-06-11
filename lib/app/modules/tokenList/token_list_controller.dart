5fb23946c9137768fff8fa149be3c34177c0faab
Kanya <kanya_ykk@hotmail.com>
2022-06-12 01:06:02 +0700

    search token list

--- lib/app/modules/tokenList/token_list_controller.dart
+++ lib/app/modules/tokenList/token_list_controller.dart
@@ -1,9 +1,13 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:swop_coin/app/models/token.dart';
 import 'package:swop_coin/app/providers/woo.dart';
 
 class TokenListController extends GetxController with StateMixin<List<Token>> {
   static TokenListController get to => Get.find();
+
+  final searchController = TextEditingController();
+  final RxList<Token> availableTokenList = RxList.empty();
 
   @override
   void onInit() {
@@ -19,8 +23,19 @@ class TokenListController extends GetxController with StateMixin<List<Token>> {
       change([], status: RxStatus.loading());
       List<Token> availableTokens = await WooProvider.to.getAvailableTokens();
       change(availableTokens, status: RxStatus.success());
+      availableTokenList(availableTokens);
     } catch (e) {
       change([], status: RxStatus.error());
     }
+  }
+
+  void updateSearchKeyword(String value) {
+    List<Token> filterTokenList = availableTokenList
+        .where((element) =>
+            element.name.toLowerCase().contains(value.toLowerCase()) ||
+            element.protocol.toLowerCase().contains(value.toLowerCase()) ||
+            element.token.toLowerCase().contains(value.toLowerCase()))
+        .toList();
+    change(filterTokenList, status: RxStatus.success());
   }
 }
--- lib/app/modules/tokenList/token_list_page.dart
+++ lib/app/modules/tokenList/token_list_page.dart
@@ -10,24 +10,40 @@ class TokenListPage extends GetWidget<TokenListController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: controller.obx(
-        (state) => ListView.separated(
-          itemCount: state!.length,
-          itemBuilder: (ctx, idx) {
-            Token token = state[idx];
-            return GestureDetector(
-                onTap: () {
-                  Get.back(result: token);
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          TextFormField(
+            controller: TokenListController.to.searchController,
+            onChanged: TokenListController.to.updateSearchKeyword,
+          ),
+          Expanded(
+            child: controller.obx(
+              (state) => ListView.separated(
+                itemCount: state!.length,
+                itemBuilder: (ctx, idx) {
+                  Token token = state[idx];
+                  return GestureDetector(
+                      onTap: () {
+                        Get.back(result: token);
+                      },
+                      child: Column(
+                        children: [
+                          Text(token.token),
+                          Text(token.protocol),
+                        ],
+                      ));
                 },
-                child: Column(
-                  children: [
-                    Text(token.token),
-                    Text(token.protocol),
-                  ],
-                ));
-          },
-          separatorBuilder: (ctx, idx) => const Divider(),
-        ),
+                separatorBuilder: (ctx, idx) => const Divider(),
+              ),
+              onEmpty: const Text('Empty'),
+              onError: (_) => const Text('Error'),
+              onLoading: const Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
